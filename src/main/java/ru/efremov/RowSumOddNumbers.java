dbac9d73961f0b18217a9af7faf4b86e70d1edd9
freshman4000 <freshman4000@gmail.com>
2019-08-13 13:46:34 +0300

    RowSumOddNumbers7kyu

---
+++ src/main/java/ru/efremov/RowSumOddNumbers.java
@@ -0,0 +1,14 @@
+package ru.efremov;
+
+public class RowSumOddNumbers {
+    public static int rowSumOddNumbers(int n) {
+        //TODO
+        int start = n * n + (n - 1);
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += start;
+            start = start - 2;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/efremov/ru/RowSumOddNumbersTest.java
@@ -0,0 +1,14 @@
+package efremov.ru;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.hamcrest.core.Is;
+import ru.efremov.RowSumOddNumbers;
+
+public class RowSumOddNumbersTest {
+    @Test
+    public void when29Then125() {
+        int sum = RowSumOddNumbers.rowSumOddNumbers(5);
+        Assert.assertThat(sum, Is.is(125));
+    }
+}
