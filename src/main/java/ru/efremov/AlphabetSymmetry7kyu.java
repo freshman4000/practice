7abc4019b65e6f3b8589cae29b207503a154d5d4
freshman4000 <freshman4000@gmail.com>
2019-08-15 23:27:00 +0300

    AlphabetSymmetry7kyu

---
+++ src/main/java/ru/efremov/AlphabetSymmetry7kyu.java
@@ -0,0 +1,27 @@
+package ru.efremov;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlphabetSymmetry7kyu {
+    public static int[] solve(String[] arr){
+        Map<Character, Integer> map = new HashMap<>();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int i = 1;
+        for (Character ch : alphabet.toCharArray()) {
+            map.put(ch, i++);
+        }
+        int[] result = new int[arr.length];
+        for (int j = 0; j < arr.length; j++) {
+            int count = 0;
+            for (int t = 0; t < arr[j].length(); t++) {
+                if (map.get(arr[j].toLowerCase().charAt(t)) == t + 1) {
+                    count++;
+                }
+            }
+            result[j] = count;
+        }
+        return result;
+    }
+}
+
---
+++ src/test/java/efremov/ru/AlphabetSymmetry7kyuTest.java
@@ -0,0 +1,14 @@
+package efremov.ru;
+
+import org.junit.Test;
+import org.junit.Assert;
+import  org.hamcrest.core.Is;
+import ru.efremov.AlphabetSymmetry7kyu;
+
+public class AlphabetSymmetry7kyuTest {
+    @Test
+    public void whenABCthen3() {
+        int[] result = AlphabetSymmetry7kyu.solve(new String [] {"abode", "ABc", "xyzD"});
+        Assert.assertThat(result, Is.is(new int[]{4, 3, 1}));
+    }
+}
